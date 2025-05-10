5c48ebd477b47ebea256797e2b99fb030f556ef2
efeliza00 <evanfeliza22@gmail.com>
2025-05-10 23:49:41 +0800

    fix: middleware not redirected after auth

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,13 @@
-export { default } from "next-auth/middleware"
+import { getToken } from "next-auth/jwt"
+import { NextResponse, type NextRequest } from "next/server"
+
+export async function middleware(request: NextRequest) {
+    const token = await getToken({ req: request })
+    if (!token && !request.nextUrl.pathname.startsWith("/login")) {
+        return NextResponse.redirect(new URL("/login", request.url))
+    }
+    return NextResponse.next()
+}
 
 export const config = {
     matcher: ["/dashboard/:path*"],
