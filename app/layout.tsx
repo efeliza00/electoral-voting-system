55212b44f7c518f131faa976e1b93189d5b0cc51
efeliza00 <evanfeliza22@gmail.com>
2025-05-10 21:58:12 +0800

    chore(app): removed font on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
 import { Toaster } from "react-hot-toast"
 import { Provider as SessionProvider } from "../lib/providers/session-provider"
 import "./globals.css"
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-})
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -27,7 +17,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`antialiased`}
             >
                 <SessionProvider>{children}</SessionProvider>
                 <Toaster
