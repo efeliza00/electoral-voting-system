51d717709e273020bad5c952ebd3e3a8845135c2
efeliza00 <evanfeliza22@gmail.com>
2025-05-16 23:22:00 +0800

    chore: setup socket.io

---
+++ server.js
@@ -0,0 +1,33 @@
+  import next from "next";
+import { createServer } from "node:http";
+
+  const dev = process.env.NODE_ENV !== "production";
+  const hostname = "localhost";
+  const port = 3000
+
+  const app = next({ dev, hostname, port });
+  const handler = app.getRequestHandler();
+
+  app.prepare().then(() => {
+    const httpServer = createServer(handler);
+
+    // const io = new Server(httpServer);
+
+    // io.on("connection", (socket) => {
+    //   console.log("Socket connected:", socket.id);
+  
+    //   socket.on("message", (msg) => {
+    //     console.log("Received message:", msg);
+    //     socket.emit("message", `Server received: ${msg}`);
+    //   });
+    // });
+
+    httpServer
+      .once("error", (err) => {
+        console.error(err);
+        process.exit(1);
+      })
+      .listen(port, () => {
+        console.log(`> Ready on http://${hostname}:${port}`);
+      });
+  });
