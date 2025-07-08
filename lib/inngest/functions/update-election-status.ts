d66ec7e8b2fb1fdad1f4826fa7a54d2ad0268daf
Evan Feliza <evanfeliza22@gmail.com>
2025-07-08 16:20:23 +0800

    style: remove trailing comma from Inngest function export

--- lib/inngest/functions/update-election-status.ts
+++ lib/inngest/functions/update-election-status.ts
@@ -1,11 +1,11 @@
 import { updateElectionStatus } from "@/app/actions/election/update-election-status";
 import { inngest } from "../client";
 
-export const updateElectionStatusCron  = inngest.createFunction(
-  { id: "electoral-voting-system" },
-  { cron: "* * * * *" },
-  async () => {
-     const res = await updateElectionStatus()
-    return { message: res?.message };
-  },
-);
+export const updateElectionStatusCron = inngest.createFunction(
+    { id: "update-election-status" },
+    { cron: "* * * * *" },
+    async () => {
+        const res = await updateElectionStatus()
+        return { message: res?.message }
+    }
+)
