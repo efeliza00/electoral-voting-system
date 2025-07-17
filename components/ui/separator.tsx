dd281a49d606ba694035aa98d0a97a49b0f3088a
Evan Feliza <evanfeliza22@gmail.com>
2025-07-17 16:01:12 +0800

    fix(separator): vertical separtor not displaying

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import * as React from "react"
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
+import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
@@ -16,10 +16,11 @@ function Separator({
             data-slot="separator-root"
             decorative={decorative}
             orientation={orientation}
-            className={cn(
-                "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
-                className
-            )}
+        className={cn(
+               "shrink-0 bg-border",
+               orientation === "horizontal" ? "h-[1px] w-full" : "min-h-full w-[1px]",
+               className,
+             )}
             {...props}
         />
     )
