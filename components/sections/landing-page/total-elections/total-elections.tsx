30d024000d33dbbadc3fed88c2f2ea5dcaf7f1ae
efeliza00 <evanfeliza22@gmail.com>
2025-07-21 20:50:20 +0800

    chore: add force dynamic

--- components/sections/landing-page/total-elections/total-elections.tsx
+++ components/sections/landing-page/total-elections/total-elections.tsx
@@ -1,15 +1,22 @@
-import { NumberTicker } from '@/components/magicui/number-ticker'
-import { Vote } from 'lucide-react'
+import { NumberTicker } from '@/components/magicui/number-ticker';
+import { Vote } from 'lucide-react';
+
 
 const TotalElections = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL!}/api/elections/count`)
   const data:number = await res.json()
   return (
-    <div className='bg-muted/50 flex-col flex items-center  justify-center py-26 '>
-      <div className='p-4 shadow bg-accent rounded-xl'><Vote  className='text-primary size-28'/></div>
-      
-      <NumberTicker value={data} className='text-primary font-semibold text-7xl leading-loose'/>
-      <h1 className='text-secondary-foreground text-2xl leading-loose'>Total Created Elections</h1>
+    <div className="bg-muted/50 flex-col flex items-center justify-center py-26">
+      <div className="p-4 shadow bg-accent rounded-xl">
+        <Vote className="text-primary size-28" />
+      </div>
+      <NumberTicker
+        value={data}
+        className="text-primary font-semibold text-7xl leading-loose"
+      />
+      <h1 className="text-secondary-foreground font-semibold text-4xl leading-loose">
+        Elections Powered by Us
+      </h1>
     </div>
   )
 }
