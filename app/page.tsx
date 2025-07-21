721c1e65b8faa4ddce5c373ad8aacc21f5e258ff
efeliza00 <evanfeliza22@gmail.com>
2025-07-21 20:58:01 +0800

    chore: add force dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,32 @@ import Navbar from "@/components/sections/landing-page/header/navbar";
 import Hero from "@/components/sections/landing-page/hero/hero-section";
 import TotalElections from "@/components/sections/landing-page/total-elections/total-elections";
 import { AdminProvider } from "@/lib/providers/session-provider";
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
 
+export const dynamic = 'force-dynamic'; // Ensure dynamic fetching
 
 export default function Home() {
-  return <>
-    <AdminProvider>
-      <Navbar />
-    </AdminProvider>
-    <Hero />
-    <TotalElections />
-    <Features />
-    <Footer />
+  return (
+    <>
+      <AdminProvider>
+        <Navbar />
+      </AdminProvider>
+      <Hero />
+      <Suspense fallback={<ElectionsCountLoader />}>
+        <TotalElections />
+      </Suspense>
+      <Features />
+      <Footer />
+    </>
+  );
 
-  </>
+}
+
+function ElectionsCountLoader() {
+  return (
+    <div className="flex items-center justify-center">
+      <Loader2 className="text-primary" />
+    </div>
+  );
 }
