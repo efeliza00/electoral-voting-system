8dcea6b157a56c5840be01170c5c8430c34e63e4
efeliza00 <evanfeliza22@gmail.com>
2025-07-22 12:06:48 +0800

    chore: fix robot disallow path typo

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,11 @@ import type { MetadataRoute } from 'next'
  
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/admind/',
-    },
-    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`
+      rules: {
+          userAgent: "*",
+          allow: "/",
+          disallow: "/admin/",
+      },
+      sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
   }
 }
