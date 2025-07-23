5d04155390e031d7023302bda8a8ece2fa2b0f10
efeliza00 <evanfeliza22@gmail.com>
2025-07-23 13:02:46 +0800

    fix(total-elections): padding on mobile screen

--- components/sections/landing-page/total-elections/total-elections.tsx
+++ components/sections/landing-page/total-elections/total-elections.tsx
@@ -6,7 +6,7 @@ const TotalElections = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL!}/api/elections/count`)
   const data:number = await res.json()
   return (
-    <div className="bg-muted/50 flex-col flex items-center justify-center p-4 md:p-0 py-26">
+    <div className="bg-muted/50 flex-col flex items-center justify-center p-4 md:px-0 py-26">
       <div className="p-4 shadow bg-accent rounded-xl">
         <Vote className="text-primary size-28" />
       </div>
